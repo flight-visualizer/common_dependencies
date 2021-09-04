9c4c97c78db7230a0243b5abeb89712a3b472de8
Michael Clark <michaeldavidclark13@gmail.com>
2021-09-04 10:45:41 -0400

    Added Request Service

---
+++ services/RequestService.py
@@ -0,0 +1,28 @@
+import requests
+from pydantic import BaseModel
+
+class RequestService():
+    """
+    Class for reading and writing from a dynamo table
+    """
+    
+    def __init__(self, endpoint: str, params: dict, responseModel: BaseModel):
+        self.endpoint = endpoint
+        self.params = params
+        self.responseModel = responseModel
+
+    def query(self):
+
+        try:
+            print(f'Attempting to query data...')
+            response = requests.get(
+                self.endpoint, 
+                params=self.params
+            )
+            print(f'Response received... status: {response.status_code}')
+
+            response = self.responseModel(**response.json())
+
+        except:
+            print('Error querying aviation stack...')
+            raise
